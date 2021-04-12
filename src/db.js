09e00bbf83d3798f20c91bd2bd8194a75cfbcd02
MatildadDS <70516554+MatildadDS@users.noreply.github.com>
2021-04-12 23:30:31 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: 'localhost',
     //user: ,
-    password: 'Pars1803?',
+    //password: ,
     database: 'todolistapp'
 });
 db.connect((error) => {
